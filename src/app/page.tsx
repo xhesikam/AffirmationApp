55d3df90bb5bc3c780211b83d502f6f05ffc9699
“Xhesika” <xhesikam2184@tywls.org>
2025-05-22 23:11:20 -0400

    small fix on title of the app

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -22,7 +22,7 @@ export default async function Home() {
     <div className=" w-screen py-20 flex justify-center flex-col items-center">
       <span className="text-4xl font-extrabold uppercase">Affirmation App</span>
       <h1 className="text-5xl font-extrabold uppercase mb-5 text-center">
-        <span className="lowercase">w/</span>Server Actions
+        <span className="lowercase">of the day</span>
       </h1>
 
       <div className="flex justify-center flex-col items-center">
